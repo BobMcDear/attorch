d5ec16e5da79670bcb52aca6a550e073babe0c67
Borna Ahmadzadeh <borna.ahz@gmail.com>
2023-09-20 10:08:58 -0400

    Feat: Add convolutional layer

---
+++ attorch/conv_kernels.py
@@ -0,0 +1,193 @@
+import triton
+import triton.language as tl
+
+
+def conv2d_forward_config(
+    BLOCK_SIZE_BATCH_HEIGHT_WIDTH: int,
+    BLOCK_SIZE_IN_FEAT: int,
+    BLOCK_SIZE_OUT_FEAT: int,
+    n_warps: int = 4,
+    n_stages: int = 2,
+    ) -> triton.Config:
+    """
+    Creates a triton.Config object for conv2d_forward_kernel
+    given meta-parameters for auto-tuning.
+
+    Args:
+        BLOCK_SIZE_BATCH_HEIGHT_WIDTH: Block size across the batch, height, and
+            width dimensions.
+        BLOCK_SIZE_IN_FEAT: Block size across the input feature dimension.
+        BLOCK_SIZE_OUT_FEAT: Block size across the output feature dimension.
+        n_warps: Number of warps to use for the kernel when compiled for GPUs.
+        n_stages: Number of stages the compiler uses to software-pipeline.
+
+    Returns:
+        Kernel configuration.
+    """
+    return triton.Config({'BLOCK_SIZE_BATCH_HEIGHT_WIDTH': BLOCK_SIZE_BATCH_HEIGHT_WIDTH,
+                          'BLOCK_SIZE_IN_FEAT': BLOCK_SIZE_IN_FEAT,
+                          'BLOCK_SIZE_OUT_FEAT': BLOCK_SIZE_OUT_FEAT},
+                          num_warps=n_warps, num_stages=n_stages)
+
+
+@triton.autotune(
+    configs=[
+        conv2d_forward_config(128, 32, 128, n_warps=8, n_stages=2),
+        conv2d_forward_config(256, 32, 64, n_warps=8, n_stages=2),
+        conv2d_forward_config(256, 32, 32, n_warps=4, n_stages=4),
+        conv2d_forward_config(256, 64, 32, n_warps=4, n_stages=4),
+        conv2d_forward_config(256, 32, 16, n_warps=2, n_stages=4),
+        conv2d_forward_config(64, 32, 128, n_warps=8, n_stages=4),
+        conv2d_forward_config(128, 32, 64, n_warps=4, n_stages=4),
+        conv2d_forward_config(64, 32, 64, n_warps=4, n_stages=4),
+        conv2d_forward_config(128, 32, 16, n_warps=4, n_stages=4),
+        conv2d_forward_config(128, 128, 128, n_warps=8, n_stages=3),
+        conv2d_forward_config(256, 128, 64, n_warps=8, n_stages=3),
+        conv2d_forward_config(256, 128, 32, n_warps=4, n_stages=4),
+        conv2d_forward_config(64, 128, 128, n_warps=4, n_stages=4),
+        conv2d_forward_config(128, 128, 64, n_warps=4, n_stages=4),
+        conv2d_forward_config(128, 64, 32, n_warps=2, n_stages=4),
+        conv2d_forward_config(64, 64, 64, n_warps=2, n_stages=4),
+    ],
+    key=['batch_dim', 'in_feat_dim', 'in_height', 'in_width',
+         'out_feat_dim', 'out_height', 'out_width',
+         'kernel_height', 'kernel_width',
+         'stride_height', 'stride_width',
+         'padding_height', 'padding_width',
+         'groups'],
+)
+@triton.jit
+def conv2d_forward_kernel(
+    input_pointer, weight_pointer, output_pointer,
+    batch_dim, in_feat_dim, in_height, in_width,
+    out_feat_dim, out_height, out_width,
+    input_batch_stride, input_in_feat_stride, input_height_stride, input_width_stride,
+    weight_out_feat_stride, weight_in_feat_stride, weight_height_stride, weight_width_stride,
+    output_batch_stride, output_out_feat_stride, output_height_stride, output_width_stride,
+    kernel_height: tl.constexpr, kernel_width: tl.constexpr,
+    stride_height: tl.constexpr, stride_width: tl.constexpr,
+    padding_height: tl.constexpr, padding_width: tl.constexpr,
+    groups: tl.constexpr,
+    BLOCK_SIZE_BATCH_HEIGHT_WIDTH: tl.constexpr, BLOCK_SIZE_IN_FEAT: tl.constexpr,
+    BLOCK_SIZE_OUT_FEAT: tl.constexpr,
+    ):
+    """
+    2D-convolves over the input using weights.
+
+    Args:
+        input_pointer: Pointer to the input to convolve over.
+            The input must be of shape [batch_dim, in_feat_dim, in_height, in_width].
+        weight_pointer: Pointer to the weights input is convolved over by.
+            The weights must be of shape [out_feat_dim, in_feat_dim, kernel_height, kernel_width].
+        output_pointer: Pointer to a container the result is written to.
+            The container must be of shape [batch_dim, out_feat_dim, out_height, out_width].
+        batch_dim: Batch dimension of the input and output.
+        in_feat_dim: Dimensionality of the input features.
+        in_height: Input height.
+        in_width: Input width.
+        out_feat_dim: Dimensionality of the output features.
+        out_height: Output height.
+        out_width: Output width.
+        input_batch_stride: Stride necessary to jump one element along the
+            input's batch dimension.
+        input_in_feat_stride: Stride necessary to jump one element along the
+            input's feature dimension.
+        input_height_stride: Stride necessary to jump one element along the
+            input's height dimension.
+        input_width_stride: Stride necessary to jump one element along the
+            input's width dimension.
+        weight_out_feat_stride: Stride necessary to jump one element along the
+            weights' output feature dimension.
+        weight_in_feat_stride: Stride necessary to jump one element along the
+            weights' input feature dimension.
+        weight_height_stride: Stride necessary to jump one element along the
+            weights' height dimension.
+        weight_width_stride: Stride necessary to jump one element along the
+            weights' width dimension.
+        output_batch_stride: Stride necessary to jump one element along the
+            output's batch dimension.
+        output_out_feat_stride: Stride necessary to jump one element along the
+            output's feature dimension.
+        output_height_stride: Stride necessary to jump one element along the
+            output's height dimension.
+        output_width_stride: Stride necessary to jump one element along the
+            output's width dimension.
+        kernel_height: Kernel height.
+        kernel_width: Kernel width.
+        stride_height: Stride of kernel across the height dimension.
+        stride_width: Stride of kernel across the width dimension.
+        padding_height: Padding applied to the input across the height dimension.
+        padding_width: Padding applied to the input across the width dimension.
+        groups: Number of groups for the convolution.
+        BLOCK_SIZE_BATCH_HEIGHT_WIDTH: Block size across the batch, height, and
+            width dimensions.
+        BLOCK_SIZE_IN_FEAT: Block size across the input feature dimension.
+        BLOCK_SIZE_OUT_FEAT: Block size across the output feature dimension.
+    """
+    batch_height_width_pid = tl.program_id(0)
+    out_feat_pid = tl.program_id(1)
+    group_pid = tl.program_id(2)
+
+    in_group_dim = in_feat_dim // groups
+    out_group_dim = out_feat_dim // groups
+
+    batch_height_width_offset = (batch_height_width_pid * BLOCK_SIZE_BATCH_HEIGHT_WIDTH +
+                                 tl.arange(0, BLOCK_SIZE_BATCH_HEIGHT_WIDTH))
+    batch_height_offset = batch_height_width_offset // out_width
+    batch_offset = batch_height_offset // out_height
+
+    output_feat_offset = (out_feat_pid * BLOCK_SIZE_OUT_FEAT +
+                          tl.arange(0, BLOCK_SIZE_OUT_FEAT))
+    output_height_offset = batch_height_offset % out_height
+    output_width_offset = batch_height_width_offset % out_width
+
+    input_pointer += (input_batch_stride * batch_offset +
+                      input_in_feat_stride * group_pid * in_group_dim)[:, None]
+    weight_pointer += (weight_out_feat_stride * output_feat_offset +
+                       weight_out_feat_stride * group_pid * out_group_dim)[None, :]
+
+    accum = tl.zeros((BLOCK_SIZE_BATCH_HEIGHT_WIDTH, BLOCK_SIZE_OUT_FEAT),
+                     dtype=tl.float32)
+
+    for h in range(kernel_height):
+        for w in range(kernel_width):
+            for c in range(0, in_group_dim, BLOCK_SIZE_IN_FEAT):
+                input_feat_offset = c + tl.arange(0, BLOCK_SIZE_IN_FEAT)
+                input_height_offset = (h - padding_height +
+                                       stride_height * output_height_offset)
+                input_width_offset = (w - padding_width +
+                                      stride_width * output_width_offset)
+
+                curr_input_pointer = (input_pointer +
+                                     (input_in_feat_stride * input_feat_offset)[None, :] +
+                                     (input_height_stride * input_height_offset)[:, None] +
+                                     (input_width_stride * input_width_offset)[:, None])
+                curr_weight_pointer = (weight_pointer +
+                                      (weight_in_feat_stride * input_feat_offset)[:, None] +
+                                      (weight_height_stride * h) +
+                                      (weight_width_stride * w))
+
+                input_mask = ((batch_offset < batch_dim)[:, None] &
+                              (input_feat_offset < in_group_dim)[None, :] &
+                              (0 <= input_height_offset)[:, None] &
+                              (input_height_offset < in_height)[:, None] &
+                              (0 <= input_width_offset)[:, None] &
+                              (input_width_offset < in_width)[:, None])
+                weight_mask = ((input_feat_offset < in_group_dim)[:, None] &
+                               (output_feat_offset < out_group_dim)[None, :])
+
+                input_block = tl.load(curr_input_pointer, mask=input_mask)
+                weight_block = tl.load(curr_weight_pointer, mask=weight_mask)
+
+                accum += tl.dot(input_block, weight_block)
+
+    output_pointer += ((output_batch_stride * batch_offset)[:, None] +
+                       (output_out_feat_stride * (group_pid * out_group_dim + output_feat_offset))[None, :] +
+                       (output_height_stride * output_height_offset)[:, None] +
+                       (output_width_stride * output_width_offset)[:, None])
+    output_mask = ((batch_offset < batch_dim)[:, None] &
+                   (output_feat_offset < out_group_dim)[None, :] &
+                   (output_height_offset < out_height)[:, None] &
+                   (output_width_offset < out_width)[:, None])
+
+    tl.store(output_pointer, accum, mask=output_mask)
---
+++ attorch/conv_layer.py
@@ -0,0 +1,220 @@
+"""
+Convolutional layer with PyTorch autodiff support.
+"""
+
+
+from typing import Optional, Tuple, Union
+
+import torch
+from torch import Tensor
+from torch import nn
+from triton import cdiv
+
+from .conv_kernels import conv2d_forward_kernel
+from .types import Context, Device
+
+
+def conv2d_output_size(
+    in_size: int,
+    kernel_size: int,
+    stride: int,
+    padding: int,
+    ) -> int:
+    """
+    Determines the output size of a 2D convolution operation.
+
+    Args:
+        in_size: Input size.
+        kernel_size: Kernel size.
+        stride: Stride.
+        padding: Padding.
+
+    Returns:
+        Output size of 2D convolution.
+    """
+    return (in_size + 2 * padding - kernel_size) // stride + 1
+
+
+class Conv2dAutoGrad(torch.autograd.Function):
+    """
+    Autodiff for 2D convolutional layer.
+    """
+    @staticmethod
+    def forward(
+        ctx: Context,
+        input: Tensor,
+        weight: Tensor,
+        bias: Optional[Tensor] = None,
+        stride_height: int = 1,
+        stride_width: int = 1,
+        padding_height: int = 1,
+        padding_width: int = 1,
+        groups: int = 1,
+        ) -> Tensor:
+        """
+        2D-convolves over the input using weights, optionally adding bias.
+
+        Args:
+            input: Input to convolve over.
+                Must be of shape [batch_dim, in_feat_dim, in_height, in_width].
+            weight: Weights input is convolved over by.
+                Must be of shape [out_feat_dim, in_feat_dim, kernel_height, kernel_width].
+            bias: Optional additive bias vector, with None for no bias.
+                If provided, must be of shape [out_feat_dim].
+            stride_height: Stride of kernel across the height dimension.
+            stride_width: Stride of kernel across the width dimension.
+            padding_height: Padding applied to the input across the height dimension.
+            padding_width: Padding applied to the input across the width dimension.
+            groups: Number of groups for the convolution.
+
+        Returns:
+            Input 2D-convolved over, potentially with added biased.
+        """
+        assert weight.ndim == 4, \
+            f'Weights must be 4D, received shape {weight.shape}'
+        assert bias is None or bias.ndim == 1, \
+            f'Bias must be 1D, received shape {bias.shape}'
+
+        assert input.shape[1] == groups * weight.shape[1], \
+            f'Incompatible input ({input.shape}) and weights ({weight.shape}) shape with {groups} groups'
+        assert bias is None or weight.shape[0] == bias.shape[0], \
+            f'Incompatible weights ({weight.shape}) and bias ({bias.shape}) shape'
+
+        batch_dim, in_feat_dim, in_height, in_width = input.shape
+        out_feat_dim, _, kernel_height, kernel_width = weight.shape
+        out_height = conv2d_output_size(in_height, kernel_height,
+                                      stride_height, padding_height)
+        out_width = conv2d_output_size(in_width, kernel_width,
+                                     stride_width, padding_width)
+
+        output = torch.empty((batch_dim, out_feat_dim, out_height, out_width),
+                             device=input.device,
+                             dtype=input.dtype)
+
+        # Launches a 3D grid, where each program outputs blocks of
+        # BLOCK_SIZE_BATCH_HEIGHT_WIDTH along the batch, height, and width dimensions,
+        # BLOCK_SIZE_OUT_FEAT along the feature dimension, and one group.
+        grid = lambda META: (cdiv(batch_dim * out_height * out_width, META['BLOCK_SIZE_BATCH_HEIGHT_WIDTH']),
+                             cdiv(out_feat_dim, META['BLOCK_SIZE_OUT_FEAT']),
+                             groups)
+        conv2d_forward_kernel[grid](input, weight, output,
+                                    batch_dim, in_feat_dim, in_height, in_width,
+                                    out_feat_dim, out_height, out_width,
+                                    *input.stride(),
+                                    *weight.stride(),
+                                    *output.stride(),
+                                    kernel_height, kernel_width,
+                                    stride_height, stride_width,
+                                    padding_height, padding_width,
+                                    groups=groups)
+
+        if bias is not None:
+            # Adding bias in the kernel becomes buggy when groups != 1.
+            output += bias.view(1, -1, 1, 1)
+
+        requires_grad = (input.requires_grad or
+                         weight.requires_grad or
+                         (bias is not None and bias.requires_grad))
+
+        ctx.stride = (stride_height, stride_width)
+        ctx.padding = (padding_height, padding_width)
+        ctx.groups = groups
+        ctx.bias_requires_grad = False if bias is None else bias.requires_grad
+        if requires_grad:
+            ctx.save_for_backward(input, weight)
+
+        return output
+
+    @staticmethod
+    def backward(
+        ctx: Context,
+        output_grad: Tensor,
+        ) -> Tuple[Optional[Tensor], ...]:
+        """
+        Calculates the input gradient of the 2D convolutional layer.
+
+        Args:
+            ctx: Context containing stored variables.
+            output_grad: Output gradients.
+                Must be the same shape as the output.
+
+        Returns:
+            Input gradient of the 2D convolutional layer.
+        """
+        input, weight = ctx.saved_tensors
+
+        # The backward pass for the convolution operation requires dilation,
+        # which is not supported by the kernel.
+        input_grad = nn.grad.conv2d_input(input.shape, weight, output_grad,
+                                          ctx.stride, ctx.padding,
+                                          groups=ctx.groups)
+        weight_grad = nn.grad.conv2d_weight(input, weight.shape, output_grad,
+                                            ctx.stride, ctx.padding,
+                                            groups=ctx.groups)
+        bias_grad = output_grad.sum(dim=(0, 2, 3)) if ctx.bias_requires_grad else None
+
+
+        return input_grad, weight_grad, bias_grad, None, None, None, None, None
+
+
+class Conv2d(nn.Conv2d):
+    """
+    2D-convolves over the input using weights, optionally adding bias.
+    See also base class.
+
+    Args:
+        in_channels: Number of input channels.
+        out_channels: Number of output channels.
+        kernel_size: Kernel size.
+            If an int, this value is used along both spatial dimensions.
+        stride: Stride of kernel.
+            If an int, this value is used along both spatial dimensions.
+        padding: Padding applied to the input.
+            If an int, this value is used along both spatial dimensions.
+        dilation: Dilation of kernel. Only 1 and (1, 1) are supported.
+        groups: Number of groups for the convolution.
+        bias: Flag for additive bias.
+        padding_mode: Padding mode. Only 'zeros' is supported.
+        device: Device to use. Only CUDA is supported.
+        dtype: Dtype of layer. Only float32 is supported.
+
+    Raises:
+        RuntimeError: 1. A dilation other than 1 was passed.
+                      2. A padding mode other than 'zeros' was passed.
+                      3. A device other than CUDA was passed.
+                      4. A dtype other than float32 was passed.
+    """
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Union[int, Tuple[int, int]],
+        stride: Union[int, Tuple[int, int]] = 1,
+        padding: Union[int, Tuple[int, int]] = 0,
+        dilation: Union[int, Tuple[int, int]] = 1,
+        groups: int = 1,
+        bias: bool = True,
+        padding_mode: str = 'zeros',
+        device: Device = 'cuda',
+        dtype: torch.dtype = torch.float32,
+        ) -> None:
+        super().__init__(in_channels, out_channels, kernel_size, stride, padding,
+                         dilation, groups, bias, padding_mode, device, dtype)
+
+        if self.dilation != (1, 1):
+            raise RuntimeError('Convolutional layer only supports dilation of 1 and (1, 1).')
+
+        if self.padding_mode != 'zeros':
+            raise RuntimeError("Convolutional layer only support 'zeros' padding mode.")
+
+        if 'cuda' not in str(device):
+            raise RuntimeError('Convolutional layer only supports CUDA devices.')
+
+        if dtype is not torch.float32:
+            raise RuntimeError('Convolutional layer only supports float32 dtype.')
+
+
+    def forward(self, input: Tensor) -> Tensor:
+        return Conv2dAutoGrad.apply(input, self.weight, self.bias,
+                                    *self.stride, *self.padding,
+                                    self.groups)
