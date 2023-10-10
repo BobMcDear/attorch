9a86eec40b03009a3a0348e2afd2279f73b3c97d
Borna Ahmadzadeh <borna.ahz@gmail.com>
2023-10-10 07:58:56 -0400

    Feat: Add support for block names in element_wise_kernel_configs

--- attorch/utils.py
+++ attorch/utils.py
@@ -8,12 +8,17 @@ from typing import List
 import triton
 
 
-def element_wise_kernel_configs() -> List[triton.Config]:
+def element_wise_kernel_configs(
+    block_name: str = 'BLOCK_SIZE',
+    ) -> List[triton.Config]:
     """
     Returns kernel configurations for element-wise operations.
+
+    Args:
+        block_name: Name of block argument rows are distributed over.
     """
-    return [triton.Config({'BLOCK_SIZE': 64}, num_warps=2),
-            triton.Config({'BLOCK_SIZE': 128}, num_warps=2),
-            triton.Config({'BLOCK_SIZE': 256}, num_warps=4),
-            triton.Config({'BLOCK_SIZE': 512}, num_warps=4),
-            triton.Config({'BLOCK_SIZE': 1024}, num_warps=4)]
+    return [triton.Config({block_name: 64}, num_warps=2),
+            triton.Config({block_name: 128}, num_warps=2),
+            triton.Config({block_name: 256}, num_warps=4),
+            triton.Config({block_name: 512}, num_warps=4),
+            triton.Config({block_name: 1024}, num_warps=4)]
