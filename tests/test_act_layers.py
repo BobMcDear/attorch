1d2f770394d5c9c83c6a199dabe886286ba61dad
Borna Ahmadzadeh <borna.ahz@gmail.com>
2024-02-08 09:02:53 -0500

    Test: Add tests for AMP activation layers

--- tests/test_act_layers.py
+++ tests/test_act_layers.py
@@ -2,6 +2,7 @@ from typing import Tuple
 
 import pytest
 from torch import nn
+from torch.cuda.amp import autocast
 
 import attorch
 from .utils import assert_close, create_input, create_input_like, default_shapes
@@ -9,15 +10,17 @@ from .utils import assert_close, create_input, create_input_like, default_shapes
 
 @pytest.mark.parametrize('shape', default_shapes())
 @pytest.mark.parametrize('act_func', ['Sigmoid', 'Tanh', 'ReLU', 'GELU', 'SiLU'])
-def test_act_layers(shape: Tuple[int, ...], act_func: str) -> None:
+@pytest.mark.parametrize('amp', [False, True])
+def test_act_layers(shape: Tuple[int, ...], act_func: str, amp: bool) -> None:
     attorch_input = create_input(shape)
     pytorch_input = create_input(shape)
 
     attorch_act_func = getattr(attorch, act_func)()
     pytorch_act_func = getattr(nn, act_func)()
 
-    attorch_output = attorch_act_func(attorch_input)
-    pytorch_output = pytorch_act_func(pytorch_input)
+    with autocast(enabled=amp):
+        attorch_output = attorch_act_func(attorch_input)
+        pytorch_output = pytorch_act_func(pytorch_input)
 
     assert_close((attorch_output, pytorch_output))
 
