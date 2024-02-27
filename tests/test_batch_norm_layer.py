0112d049d9f17b32d909f3b9215fcb2677eb58b9
Borna Ahmadzadeh <borna.ahz@gmail.com>
2024-02-27 09:26:23 -0500

    Test: Add tests for batch norm with residual addition

--- tests/test_batch_norm_layer.py
+++ tests/test_batch_norm_layer.py
@@ -16,6 +16,7 @@ from .utils import assert_close, create_input, create_input_like, default_shapes
 @pytest.mark.parametrize('momentum', [0.1, 0.2])
 @pytest.mark.parametrize('affine', [False, True])
 @pytest.mark.parametrize('track_running_stats', [False, True])
+@pytest.mark.parametrize('add_pre_act', [False, True])
 @pytest.mark.parametrize('act_func', [None, 'sigmoid', 'tanh', 'relu', 'gelu', 'silu'])
 @pytest.mark.parametrize('input_dtype', [torch.float32, torch.float16])
 @pytest.mark.parametrize('amp', [False, True])
@@ -25,6 +26,7 @@ def test_batch_norm_layer(
     momentum: float,
     affine: bool,
     track_running_stats: bool,
+    add_pre_act: bool,
     act_func: Optional[str],
     input_dtype: bool,
     amp: bool,
@@ -35,6 +37,13 @@ def test_batch_norm_layer(
     bn_name = 'BatchNorm2d' if len(shape) == 4 else 'BatchNorm1d'
     attorch_input = create_input(shape, dtype=input_dtype)
     pytorch_input = create_input(shape, dtype=input_dtype)
+
+    if add_pre_act:
+        attorch_residual = create_input(shape, dtype=input_dtype, seed=1)
+        pytorch_residual = create_input(shape, dtype=input_dtype, seed=1)
+
+    else:
+        attorch_residual = pytorch_residual = None
 
     attorch_batch_norm = getattr(attorch, bn_name)(num_features=shape[1],
                                                    eps=eps, momentum=momentum,
@@ -58,8 +67,14 @@ def test_batch_norm_layer(
         init.normal_(pytorch_batch_norm.bias)
 
     with autocast(enabled=amp):
-        attorch_output = attorch_batch_norm(attorch_input)
-        pytorch_output = pytorch_act(pytorch_batch_norm(pytorch_input))
+        if add_pre_act:
+            attorch_output = attorch_batch_norm(attorch_input, attorch_residual)
+            pytorch_output = pytorch_act(pytorch_batch_norm(pytorch_input) +
+                                         pytorch_residual)
+
+        else:
+            attorch_output = attorch_batch_norm(attorch_input)
+            pytorch_output = pytorch_act(pytorch_batch_norm(pytorch_input))
 
     assert_close((attorch_output, pytorch_output),
                  (attorch_batch_norm.running_mean, pytorch_batch_norm.running_mean),
@@ -68,20 +83,28 @@ def test_batch_norm_layer(
     attorch_output.backward(create_input_like(attorch_output))
     pytorch_output.backward(create_input_like(pytorch_output))
 
+    residual_grad_pair = ((attorch_residual.grad, pytorch_residual.grad)
+                          if add_pre_act else (None, None))
     weight_grad_pair = ((attorch_batch_norm.weight.grad, pytorch_batch_norm.weight.grad)
                         if affine else (None, None))
     bias_grad_pair = ((attorch_batch_norm.bias.grad, pytorch_batch_norm.bias.grad)
                       if affine else (None, None))
     assert_close((attorch_input.grad, pytorch_input.grad),
-                 weight_grad_pair, bias_grad_pair,
+                 residual_grad_pair, weight_grad_pair, bias_grad_pair,
                  rtol=1e-2, atol=1e-3)
 
     attorch_batch_norm.eval()
     pytorch_batch_norm.eval()
 
     with autocast(enabled=amp):
-        attorch_output = attorch_batch_norm(attorch_input)
-        pytorch_output = pytorch_act(pytorch_batch_norm(pytorch_input))
+        if add_pre_act:
+            attorch_output = attorch_batch_norm(attorch_input, attorch_residual)
+            pytorch_output = pytorch_act(pytorch_batch_norm(pytorch_input) +
+                                         pytorch_residual)
+
+        else:
+            attorch_output = attorch_batch_norm(attorch_input)
+            pytorch_output = pytorch_act(pytorch_batch_norm(pytorch_input))
 
     assert_close((attorch_output, pytorch_output),
                  (attorch_batch_norm.running_mean, pytorch_batch_norm.running_mean),
