f4d2c487f03cd902054420e26ad559ed1eeaa36d
Borna Ahmadzadeh <borna.ahz@gmail.com>
2024-03-03 09:41:21 -0500

    Test: Add tests for AMP activation layers

--- tests/test_act_layers.py
+++ tests/test_act_layers.py
@@ -1,6 +1,7 @@
 from typing import Tuple
 
 import pytest
+import torch
 from torch import nn
 from torch.cuda.amp import autocast
 
@@ -10,10 +11,19 @@ from .utils import assert_close, create_input, create_input_like, default_shapes
 
 @pytest.mark.parametrize('shape', default_shapes())
 @pytest.mark.parametrize('act_func', ['Sigmoid', 'Tanh', 'ReLU', 'GELU', 'SiLU'])
+@pytest.mark.parametrize('input_dtype', [torch.float32, torch.float16])
 @pytest.mark.parametrize('amp', [False, True])
-def test_act_layers(shape: Tuple[int, ...], act_func: str, amp: bool) -> None:
-    attorch_input = create_input(shape)
-    pytorch_input = create_input(shape)
+def test_act_layers(
+    shape: Tuple[int, ...],
+    act_func: str,
+    input_dtype: bool,
+    amp: bool,
+    ) -> None:
+    if input_dtype is torch.float16 and not amp:
+        return
+
+    attorch_input = create_input(shape, dtype=input_dtype)
+    pytorch_input = create_input(shape, dtype=input_dtype)
 
     attorch_act_func = getattr(attorch, act_func)()
     pytorch_act_func = getattr(nn, act_func)()
@@ -22,9 +32,9 @@ def test_act_layers(shape: Tuple[int, ...], act_func: str, amp: bool) -> None:
         attorch_output = attorch_act_func(attorch_input)
         pytorch_output = pytorch_act_func(pytorch_input)
 
-    assert_close((attorch_output, pytorch_output))
+    assert_close((attorch_output, pytorch_output), rtol=1e-3, atol=1e-3)
 
     attorch_output.backward(create_input_like(attorch_output))
     pytorch_output.backward(create_input_like(pytorch_output))
 
-    assert_close((attorch_input.grad, pytorch_input.grad))
+    assert_close((attorch_input.grad, pytorch_input.grad), rtol=1e-3, atol=1e-3)
