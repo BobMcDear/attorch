88bcfcaa341a78ffd19026578530d3b0eca7e4b1
Borna Ahmadzadeh <borna.ahz@gmail.com>
2024-03-08 08:40:58 -0500

    Fix: Add missing allow_tf32 utility function

--- attorch/utils.py
+++ attorch/utils.py
@@ -9,6 +9,13 @@ import torch
 import triton
 
 
+def allow_tf32() -> bool:
+    """
+    Returns whether the current GPU architecture supports TF32.
+    """
+    return torch.cuda.get_device_capability()[0] >= 8
+
+
 def get_n_stages(n_stages: int = 2) -> int:
     """
     Receives number of stages for software pipelining and returns it as-is
