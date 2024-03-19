97d5426a6b02d8e246ac360b9c833897d477d8be
Borna Ahmadzadeh <borna.ahz@gmail.com>
2024-03-19 08:01:23 -0400

    Test: Add tests for AMP linear layer

--- tests/test_linear_layer.py
+++ tests/test_linear_layer.py
@@ -3,6 +3,7 @@ from typing import Optional, Tuple
 import pytest
 import torch
 from torch import nn
+from torch.cuda.amp import autocast
 from torch.nn import functional as F
 
 import attorch
@@ -12,15 +13,22 @@ from .utils import assert_close, create_input, create_input_like, default_shapes
 @pytest.mark.parametrize('input_shape', default_shapes(min_dim=2, max_dim=3))
 @pytest.mark.parametrize('out_dim', [16, 96, 128, 196, 384, 512, 768, 1024])
 @pytest.mark.parametrize('bias', [False, True])
-@pytest.mark.parametrize('act_func', [None, 'sigmoid', 'tanh', 'relu', 'gelu'])
+@pytest.mark.parametrize('act_func', [None, 'sigmoid', 'tanh', 'relu', 'gelu', 'silu'])
+@pytest.mark.parametrize('input_dtype', [torch.float32, torch.float16])
+@pytest.mark.parametrize('amp', [False, True])
 def test_linear_layer(
     input_shape: Tuple[int, ...],
     out_dim: int,
     bias: bool,
     act_func: Optional[str],
+    input_dtype: bool,
+    amp: bool,
     ) -> None:
-    attorch_input = create_input(input_shape)
-    pytorch_input = create_input(input_shape)
+    if input_dtype is torch.float16 and not amp:
+        return
+
+    attorch_input = create_input(input_shape, dtype=input_dtype)
+    pytorch_input = create_input(input_shape, dtype=input_dtype)
 
     torch.manual_seed(0)
     attorch_linear = attorch.Linear(input_shape[-1], out_dim,
@@ -31,8 +39,9 @@ def test_linear_layer(
                                bias=bias, device='cuda')
     pytorch_act = nn.Identity() if act_func is None else getattr(F, act_func)
 
-    attorch_output = attorch_linear(attorch_input)
-    pytorch_output = pytorch_act(pytorch_linear(pytorch_input))
+    with autocast(enabled=amp):
+        attorch_output = attorch_linear(attorch_input)
+        pytorch_output = pytorch_act(pytorch_linear(pytorch_input))
 
     assert_close((attorch_output, pytorch_output), rtol=1e-3, atol=1e-3)
 
@@ -40,7 +49,7 @@ def test_linear_layer(
     pytorch_output.backward(create_input_like(pytorch_output))
 
     bias_grad_pair = ((attorch_linear.bias.grad, pytorch_linear.bias.grad)
-                      if bias else (torch.tensor(0), torch.tensor(0)))
+                      if bias else (None, None))
     assert_close((attorch_input.grad, pytorch_input.grad),
                  (attorch_linear.weight.grad, pytorch_linear.weight.grad.T.contiguous()),
                  bias_grad_pair, rtol=1e-3, atol=1e-3)
