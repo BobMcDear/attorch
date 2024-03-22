5e63dd2ec245990a1764a08b5ab97b47b08193da
Borna Ahmadzadeh <borna.ahz@gmail.com>
2024-03-22 08:06:48 -0400

    Test: Add tests for AMP softmax

--- tests/test_softmax_layers.py
+++ tests/test_softmax_layers.py
@@ -1,7 +1,9 @@
 from typing import Tuple
 
 import pytest
+import torch
 from torch import nn
+from torch.cuda.amp import autocast
 
 import attorch
 from .utils import assert_close, create_input, create_input_like, default_shapes
@@ -9,15 +11,26 @@ from .utils import assert_close, create_input, create_input_like, default_shapes
 
 @pytest.mark.parametrize('shape', default_shapes())
 @pytest.mark.parametrize('softmax', ['Softmax', 'LogSoftmax', 'Softmin'])
-def test_softmax_layers(shape: Tuple[int, ...], softmax: str) -> None:
-    attorch_input = create_input(shape)
-    pytorch_input = create_input(shape)
+@pytest.mark.parametrize('input_dtype', [torch.float32, torch.float16])
+@pytest.mark.parametrize('amp', [False, True])
+def test_softmax_layers(
+    shape: Tuple[int, ...],
+    softmax: str,
+    input_dtype: bool,
+    amp: bool,
+    ) -> None:
+    if input_dtype is torch.float16 and not amp:
+        return
+
+    attorch_input = create_input(shape, dtype=input_dtype)
+    pytorch_input = create_input(shape, dtype=input_dtype)
 
     attorch_softmax = getattr(attorch, softmax)(dim=-1)
     pytorch_softmax = getattr(nn, softmax)(dim=-1)
 
-    attorch_output = attorch_softmax(attorch_input)
-    pytorch_output = pytorch_softmax(pytorch_input)
+    with autocast(enabled=amp):
+        attorch_output = attorch_softmax(attorch_input)
+        pytorch_output = pytorch_softmax(pytorch_input)
 
     assert_close((attorch_output, pytorch_output))
 
