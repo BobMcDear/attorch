15c84c734131be9882956693e03ab5b4f9ee20ad
Borna Ahmadzadeh <borna.ahz@gmail.com>
2024-04-29 08:09:22 -0400

    Feat: Add fused dropout support to activation layers

--- attorch/act_layers.py
+++ attorch/act_layers.py
@@ -1,9 +1,10 @@
 """
-Activation function layers with PyTorch autodiff support.
+Activation function layers with fused dropout with PyTorch autodiff support.
 """
 
 
 import warnings
+from random import randint
 from typing import Optional, Tuple
 
 import torch
@@ -26,9 +27,11 @@ class ActFuncAutoGrad(torch.autograd.Function):
         ctx: Context,
         input: Tensor,
         act_func: str,
+        drop_p: float,
+        training: bool,
         ) -> Tensor:
         """
-        Applies an activation function to the input.
+        Applies an activation function to the input, optionally fusing dropout.
 
         Args:
             ctx: Context for variable storage.
@@ -36,11 +39,20 @@ class ActFuncAutoGrad(torch.autograd.Function):
                 Can have arbitrary shape.
             act_func: Name of activation function to apply.
                 Options are 'sigmoid', 'tanh', 'relu', 'gelu', and 'silu'.
+            drop_p: Probability of dropping an element for dropout.
+            training: Flag indicating if the model is in training mode,
+                only applicable if drop_p is greater than 0.
+                If False, no dropout is applied.
 
         Returns:
-            Input transformed by the desired activation function.
+            Input transformed by the desired activation function,
+            potentially with fused dropout.
         """
         ctx.act_func = act_func
+        ctx.drop_p = drop_p
+        ctx.dropout = drop_p > 0 and training
+        seed = randint(0, 65535) if ctx.dropout else 0
+        ctx.seed = seed
         if input.requires_grad:
             ctx.save_for_backward(input)
 
@@ -51,7 +63,9 @@ class ActFuncAutoGrad(torch.autograd.Function):
         # Launches 1D grid where each program operates over
         # BLOCK_SIZE elements.
         grid = lambda META: (cdiv(size, META['BLOCK_SIZE']),)
-        act_func_forward_kernel[grid](flattened_input, output, size, act_func)
+        act_func_forward_kernel[grid](flattened_input, output, size,
+                                      drop_p, seed,
+                                      act_func, ctx.dropout)
 
         return output.view_as(input)
 
@@ -83,75 +97,101 @@ class ActFuncAutoGrad(torch.autograd.Function):
         # BLOCK_SIZE elements.
         grid = lambda META: (cdiv(size, META['BLOCK_SIZE']),)
         act_func_backward_kernel[grid](output_grad, flattened_input, input_grad,
-                                       size, ctx.act_func)
+                                       size, ctx.drop_p, ctx.seed,
+                                       ctx.act_func, ctx.dropout)
 
         # Pads output with None because a gradient is necessary for
         # all input arguments.
-        return input_grad.view_as(input), None
+        return input_grad.view_as(input), None, None, None
 
 
 class Sigmoid(nn.Sigmoid):
     """
-    Applies sigmoid to the input.
+    Applies sigmoid to the input, optionally fusing dropout.
     See also base class.
+
+    Args:
+        drop_p: Probability of dropping an element for dropout.
     """
+    def __init__(self, drop_p: float = 0.0) -> None:
+        super().__init__()
+        self.drop_p = drop_p
+
     def forward(self, input: Tensor) -> Tensor:
-        return ActFuncAutoGrad.apply(input, 'sigmoid')
+        return ActFuncAutoGrad.apply(input, 'sigmoid', self.drop_p, self.training)
 
 
 class Tanh(nn.Tanh):
     """
-    Applies tanh to the input.
+    Applies tanh to the input, optionally fusing dropout.
     See also base class.
+
+    Args:
+        drop_p: Probability of dropping an element for dropout.
     """
+    def __init__(self, drop_p: float = 0.0) -> None:
+        super().__init__()
+        self.drop_p = drop_p
+
     def forward(self, input: Tensor) -> Tensor:
-        return ActFuncAutoGrad.apply(input, 'tanh')
+        return ActFuncAutoGrad.apply(input, 'tanh', self.drop_p, self.training)
 
 
 class ReLU(nn.ReLU):
     """
-    Applies ReLU to the input.
+    Applies ReLU to the input, optionally fusing dropout.
     See also base class.
 
     Args:
         inplace: This is a dummy argument and has no effects,
             as in-place is currently not supported.
+        drop_p: Probability of dropping an element for dropout.
     """
-    def __init__(self, inplace: bool = False) -> None:
+    def __init__(self, inplace: bool = False, drop_p: float = 0.0) -> None:
         super().__init__(inplace=False)
+        self.drop_p = drop_p
 
         if inplace is True:
             warnings.warn('In-place ReLU currently not supported; '
                           'falling back to out-of-place.')
 
     def forward(self, input: Tensor) -> Tensor:
-        return ActFuncAutoGrad.apply(input, 'relu')
+        return ActFuncAutoGrad.apply(input, 'relu', self.drop_p, self.training)
 
 
 class GELU(nn.GELU):
     """
-    Applies GELU to the input.
+    Applies GELU to the input, optionally fusing dropout.
     See also base class.
+
+    Args:
+        drop_p: Probability of dropping an element for dropout.
     """
+    def __init__(self, drop_p: float = 0.0) -> None:
+        super().__init__()
+        self.drop_p = drop_p
+
     def forward(self, input: Tensor) -> Tensor:
-        return ActFuncAutoGrad.apply(input, 'gelu')
+        return ActFuncAutoGrad.apply(input, 'gelu', self.drop_p, self.training)
 
 
 class SiLU(nn.SiLU):
     """
-    Applied SiLU to the input.
+    Applied SiLU to the input, optionally fusing dropout.
     See also base class.
 
     Args:
         inplace: This is a dummy argument and has no effects,
             as in-place is currently not supported.
+        drop_p: Probability of dropping an element for dropout.
     """
-    def __init__(self, inplace: bool = False) -> None:
+    def __init__(self, inplace: bool = False, drop_p: float = 0.0) -> None:
         super().__init__(inplace=False)
+        self.drop_p = drop_p
 
         if inplace is True:
             warnings.warn('In-place SiLU currently not supported; '
                           'falling back to out-of-place.')
 
     def forward(self, input: Tensor) -> Tensor:
-        return ActFuncAutoGrad.apply(input, 'silu')
+        return ActFuncAutoGrad.apply(input, 'silu', self.drop_p, self.training)
