78c711c6fb0e6fe69b2a5c62aac3fe55c1b72141
HUSEIN ZOLKEPLI <husein.zol05@gmail.com>
2024-05-03 01:15:06 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="attorch",
+    version="0.1",
+    author="BobMcDear",
+    url="https://github.com/BobMcDear/attorch",
+    packages=find_packages(),
+)
